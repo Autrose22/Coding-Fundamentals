0b530ad1670db18daffcec295472f2f68e62fbca
autumnrose <Arosebarrios95@gmail.com>
2021-12-18 18:13:33 -0600

    added stuff

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. myFunction()", ".d createMyFunction()"],
+        answer: "b. function myFunction()"
+    },
+]
