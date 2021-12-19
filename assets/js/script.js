c9fc634bb5dba216f91953428dc61e830d5178db
autumnrose <Arosebarrios95@gmail.com>
2021-12-18 20:47:33 -0600

    added more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,75 @@
 const questions = [
     {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["a. function = myFunction()", "b. function myFunction()", "c. myFunction()", ".d createMyFunction()"],
-        answer: "b. function myFunction()"
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
     },
-]
+
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"  
+    },
+
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+
+    {
+        question: "How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c. myFunctions()"
+    },
+
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
+
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a. onclick"
+    }
+
+    {
+        question: "How do you add a comment in a HTML?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "b. <!--This is a comment-->"
+    },
+
+    {
+        question: "How do you add a comment in a JavaScript?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "a. //This is a comment"
+    },
+
+    {
+        question: "What does HTML stand for?",
+        choices: ["a. Hypertext Mixing Language", "b. Hippos Math Lesson ", "c. Hopping Markup Land", "d. Hypertext Markup Language"],
+        answer: "d. Hypertext Markup Language"
+    },
+];
+
+var time = document.getElementById("time");
+var timesUp = document.getElementById("timesUp");
+var timeLeft = document.getElementById("timeLeft");
+
+var startQuizBtn = document.getElementById("start-quiz-button");
+var startDiv = document.getElementById("start");
+
+var totalTime = 151;
+function newQuiz() {
+    questionIndex = 0;
+    totalTime = 150;
+    timeLeft.textContent = totalTime;
+    initialInput.textContent = "";
